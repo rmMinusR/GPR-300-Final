d6bee0dde1bd9ced57481146800c19a78bd990cb
Robert Christensen <robert.christensen@mymail.champlain.edu>
2022-05-06 21:01:29 -0400

    Added mesh manipulation compute shader

--- Final/Source/MeshFromHeightmap/MeshFromHeightmap.Build.cs
+++ Final/Source/MeshFromHeightmap/MeshFromHeightmap.Build.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnrealBuildTool;
 
 public class MeshFromHeightmap : ModuleRules
@@ -5,6 +6,16 @@ public class MeshFromHeightmap : ModuleRules
 	public MeshFromHeightmap(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-	}	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RenderCore", "Renderer", "RHI" });
+		PublicIncludePathModuleNames.Add("MeshFromHeightmap");
+	}
+
+	/*
+	public override void SetupBinaries(TargetInfo Target,
+		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfiguration,
+		ref List<string> OutExtraModuleNames)
+	{
+		
+	}
+	// */
 }
